d08bf07903fa1142d2cd41702a72ae10a2ff5d0a
jachtomadorota <jachtoma.do@gmail.com>
2021-03-18 13:01:51 +0100

    add new tests with Mono presentation of work

--- src/main/java/MonoTest.java
+++ src/main/java/MonoTest.java
@@ -18,7 +18,33 @@ public class MonoTest {
     public void emptyMono(){
         Mono.empty()
                 .log()
-                .subscribe(System.out::println);
+                .subscribe(System.out::println,null, () -> System.out.println("Done"));
+    }
+
+    //unchecked exception
+
+    @Test
+    public void errorMono(){
+        Mono.error(new RuntimeException())
+                .log()
+                .subscribe();
+    }
+
+    //checked exception - try..catch resources doesn't work in reactive programming
+
+    @Test
+    public void errorMonoChecked(){
+        Mono.error(new Exception())
+                .log()
+                .subscribe(System.out::println, e -> System.out.println("Error " + e));
+    }
+
+    @Test
+    public void errorDo(){
+        Mono.error(new Exception())
+                .doOnError(e -> System.out.println("Error " + e))
+                .log()
+                .subscribe();
     }
 
 }
